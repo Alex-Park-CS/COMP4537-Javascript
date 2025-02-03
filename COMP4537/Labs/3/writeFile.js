613d7429139918c242c13f5e18b3a7f9a98b11ee
Alex-Park-CS <alexpark0930@gmail.com>
2025-02-02 20:35:27 -0800

    delete file.txt and fix path of writeFile.js

--- COMP4537/Labs/3/writeFile.js
+++ COMP4537/Labs/3/writeFile.js
@@ -4,25 +4,32 @@ const fs = require('fs');
 
 http.createServer(function (req, res) {
     const parsedUrl = url.parse(req.url, true);
-    
+    const pathname = parsedUrl.pathname;
+
     // Ignore favicon requests
-    if (parsedUrl.pathname === "/favicon.ico") {
+    if (pathname === "/favicon.ico") {
         res.writeHead(204, { 'Content-Type': 'image/x-icon' });
-        return res.end(); 
+        return res.end();
     }
 
     const text = parsedUrl.query.text || '';
     console.log("Text Test: " + text);
+    console.log("Pathname: " + pathname); 
 
-    fs.appendFile('file.txt', text, function (err) {
-        if (err) {
-            res.writeHead(500, {'Content-Type': 'text/html'});
-            res.end("Error saving file!");
-            throw err;
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end("File has been saved!");
-    });
+    if (pathname.includes('/COMP4537/labs/3/writeFile')) {
+        fs.appendFile('file.txt', text + '\n', function (err) {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end("Error saving file!");
+                throw err;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("File has been saved!");
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(`Error 404: Path not found - ${pathname}`);
+    }
 }).listen(8888);
 
 console.log("Server is running and listening on port 8888...");
